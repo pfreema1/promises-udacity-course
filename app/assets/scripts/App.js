4a9419044cdf42d58dddb3aa90fa0056310bf465
Paul <pfreema1@gmail.com>
2018-01-22 16:17:54 -0500

    hoooboy

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,48 @@
-console.log("ᕕ( ᐛ )ᕗ -- weeeeeee!");
+//write a function that finishes at a random time
+
+function asyncFunction(str) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(str);
+    }, (Math.floor(Math.random() * 3000) + 500));
+  });
+
+  // setTimeout(() => {
+  //   console.log(str);
+  //   return str;
+  // }, (Math.floor(Math.random() * 3000) + 500));
+}
+
+let promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("feets");
+  }, (Math.floor(Math.random() * 3000) + 500));
+});
+
+let fakeData = ["styrofoam", "paper", "phone", "keyboard", "macintosh", "woohoo", "quotes", "poo"];
+
+let promises = [];
+
+
+fakeData.forEach((item) => {
+  promises.push(asyncFunction(item));
+});
+
+// setTimeout(() => {
+//   console.log(promises);
+// }, 7000);
+
+
+Promise.all(promises).then((values) => {
+  console.log("all done!");
+  console.log(values);
+});
+
+// promise1.then(res => {
+//   console.log(res);
+// });
+
+
+// let finishedArray = fakeData.map(elem => {
+//   asyncFunction(elem);
+// });
